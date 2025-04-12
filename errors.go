e442030456bbdeda7e575cebe0695d85203249cc
SmartWalle <smartwalle@gmail.com>
2025-04-12 13:08:25 +0800

    update

--- errors.go
+++ errors.go
@@ -2,96 +2,27 @@ package errors
 
 import (
 	"bytes"
-	"encoding/json"
-	"fmt"
-	"runtime"
 	"strconv"
-	"strings"
 )
 
-const (
-	sep = " - "
-)
-
-func New(code int32, message string) *Error {
+func New(code int, message string) *Error {
 	var err = &Error{}
 	err.Code = code
 	err.Message = message
 	return err
 }
 
-func Parse(s string) *Error {
-	var e *Error
-
-	var bytes = []byte(s)
-
-	if bytes[0] == '{' {
-		if err := json.Unmarshal(bytes, &e); err != nil {
-			return New(0, s)
-		}
-	} else {
-		var ss = strings.SplitN(s, sep, 2)
-		if len(ss) > 1 {
-			var code, _ = strconv.Atoi(ss[0])
-			return New(int32(code), ss[1])
-		}
-		return New(0, s)
-	}
-	return e
-}
-
 type Error struct {
-	Data    interface{} `json:"data,omitempty"`
+	Code    int         `json:"code"`
+	Status  int         `json:"status"`
 	Message string      `json:"message,omitempty"`
-	File    string      `json:"file,omitempty"`
-	Func    string      `json:"func,omitempty"`
-	Line    int         `json:"line,omitempty"`
-	Code    int32       `json:"code"`
+	Data    interface{} `json:"data,omitempty"`
 }
 
 func (e *Error) Error() string {
 	var buf bytes.Buffer
-	//if e.File != "" {
-	//	buf.WriteString(fmt.Sprintf("[%s - %s : %d] ", e.File, e.Func, e.Line))
-	//}
-	buf.WriteString(fmt.Sprintf("%d", e.Code))
-	buf.WriteString(sep)
+	buf.WriteString(strconv.Itoa(e.Code))
+	buf.WriteByte('-')
 	buf.WriteString(e.Message)
 	return buf.String()
-
-	//bytes, _ := json.Marshal(e)
-	//return string(bytes)
-}
-
-func (e *Error) Format(args ...interface{}) *Error {
-	var ne = *e
-	ne.Message = fmt.Sprintf(e.Message, args...)
-	return &ne
-}
-
-func (e *Error) Location() *Error {
-	pc, file, line, ok := runtime.Caller(1)
-	if ok == false {
-		file = "???"
-		line = -1
-	}
-	f := runtime.FuncForPC(pc)
-
-	var ne = *e
-	ne.File = file
-	ne.Line = line
-	ne.Func = f.Name()
-	return &ne
-}
-
-func (e *Error) WithData(data interface{}) *Error {
-	var ne = *e
-	ne.Data = data
-	return &ne
-}
-
-func (e *Error) WithMessage(msg string) *Error {
-	var ne = *e
-	ne.Message = msg
-	return &ne
 }
--- examples/main.go
+++ examples/main.go
@@ -1,24 +1,4 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"github.com/smartwalle/errors"
-)
-
 func main() {
-	var e1 = errors.New(1, "error1")
-	fmt.Println(e1)
-	var e2 = errors.Parse(e1.Error())
-	fmt.Println(e2)
-
-	var e3 = errors.New(3, "error3").Location().WithData("sss")
-
-	fmt.Println(e3)
-
-	mb, _ := json.Marshal(e3)
-	fmt.Println(string(mb))
-
-	var e5 = errors.Parse(`{"code":3,"message":"error3","file":"/Users/yang/go/src/github.com/smartwalle/errors/sample/main.go","line":12,"func":"main.main"}`)
-	fmt.Println(e5)
 }
